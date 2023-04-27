2d1a2aa55abac9efdf9c77a19da2b4bc08202357
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-04-27 11:03:27 +0200

    update: alle classes die we moeten gebruiken added

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include_once(__DIR__ . "/bootstrap.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AIAI-PROMPT</title>
+    <title>PromptPlaza</title>
 </head>
 
 <body>
---
+++ src/PromptPlaza/Framework/Report.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace PromptPlaza\Framework;
+
+class Report
+{
+
+}
