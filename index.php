485afcfa62973cc3d861d6e78ff7ee98745745a1
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-04-28 15:30:58 +0200

    feat: #8 - As a user I want to see a list of newly created prompts in a chronological list to see what new prompts are.

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-include_once(__DIR__ . "/bootstrap.php");
+    include_once(__DIR__ . "/bootstrap.php");
 
-
-  if($_SESSION['loggedin'] !== true){
-    header("Location: login.php");
-  }
-
+    $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+    $offset = ($page - 1) * 10;
+    $prompts = \PromptPlaza\Framework\Prompt::getAll($offset);
+    $totalPrompts = \PromptPlaza\Framework\Prompt::countAll();
+    $totalPages = ceil($totalPrompts / 10);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +18,44 @@ include_once(__DIR__ . "/bootstrap.php");
 </head>
 
 <body>
-    <?php include_once("nav.inc.php"); ?>
+        <?php if($_SESSION['loggedin'] !== true): ?>
+            <a href="login.php">login</a>
+        <?php else: ?>
+            <?php include_once("nav.inc.php"); ?>
+        <?php endif; ?>
     <h1>Homepage</h1>
+
+    <div class="prompts">
+        <?php foreach($prompts as $prompt): ?>
+            <div class="prompt"> 
+                <?php if($_SESSION['loggedin'] !== true): ?>
+                    <p><strong><?php echo htmlspecialchars($prompt['email']);?></strong> <?php echo htmlspecialchars(substr($prompt['prompt'], 0, 20)) . '...';?></p>
+                <?php else: ?>
+                    <p><strong><?php echo htmlspecialchars($prompt['email']);?></strong> <?php echo htmlspecialchars($prompt['prompt']);?></p>
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    
+    <!-- Teller om van pagina te veranderen voor volgende prompts te zien -->
+    <div class="pagination">
+            <?php if($page > 1): ?>
+                <a href="?page=<?php echo $page - 1; ?>">Previous</a>
+            <?php endif; ?>
+
+            <?php for($i = 1; $i <= $totalPages; $i++): ?>
+                <?php if($i === $page): ?>
+                    <span><?php echo $i; ?></span>
+                <?php else: ?>
+                    <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                <?php endif; ?>
+            <?php endfor; ?>
+
+            <?php if($page < $totalPages): ?>
+                <a href="?page=<?php echo $page + 1; ?>">Next</a>
+            <?php endif; ?>
+        </div>
+
 </body>
 
 </html>
--- src/PromptPlaza/Framework/Prompt.php
+++ src/PromptPlaza/Framework/Prompt.php
@@ -4,5 +4,30 @@ namespace PromptPlaza\Framework;
 
 class Prompt
 {
+    public int $id;
+    public string $prompt;
+    public int $user_id;
 
+    //haalt alle prompts op en geeft ze terug
+    public static function getAll($offset = 0){
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("
+            SELECT prompts.*, users.email 
+            FROM prompts 
+            JOIN users ON prompts.user_id = users.id 
+            ORDER BY prompts.id DESC
+            LIMIT 10 OFFSET :offset"
+        );
+        $statement->bindValue(":offset", (int) $offset, \PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    //telt alle prompts en geeft het aantal terug
+    public static function countAll(){
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("SELECT COUNT(*) FROM prompts");
+        $statement->execute();
+        return $statement->fetchColumn();
+    }
 }
