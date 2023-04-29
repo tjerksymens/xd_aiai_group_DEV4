69ec2449bcf62a0fee1e850d5969592134e17f63
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-04-29 21:40:14 +0200

    #12 - As a user I want to be able to upload a new prompt

--- index.php
+++ index.php
@@ -6,6 +6,55 @@
     $prompts = \PromptPlaza\Framework\Prompt::getAll($offset);
     $totalPrompts = \PromptPlaza\Framework\Prompt::countAll();
     $totalPages = ceil($totalPrompts / 10);
+
+    //prompt toevoegen
+    if(!empty($_POST)){
+        //img upload en check
+        if(isset($_FILES['image'])){
+            $img_name = $_FILES['image']['name'];
+            $img_size = $_FILES['image']['size'];
+            $tmp_name = $_FILES['image']['tmp_name'];
+            $img_error = $_FILES['image']['error'];
+
+            if ($img_error === 0) {
+                if ($img_size > 1000000) {
+                    $error = "Sorry, your file is too large.";
+                } else {
+                    $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+                    $img_ex_lc = strtolower($img_ex);
+
+                    $allowed_exs = array("jpg", "jpeg", "png");
+
+                    if (in_array($img_ex_lc, $allowed_exs)) {
+                        $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+                        $img_upload_path = (__DIR__ . "/uploads/") . $new_img_name;
+                        move_uploaded_file($tmp_name, $img_upload_path);
+                    } else {
+                        $error = "You can't upload files of this type.";
+                    }
+                }
+            } else {
+                $error = "Unknown error occurred.";
+            }
+        }
+        else{
+            $error = "No image selected.";
+        }
+        
+        try{
+            $prompt = new \PromptPlaza\Framework\Prompt();
+            $prompt->setPrompt($_POST['prompt']);
+            $prompt->setImage($new_img_name);
+            $prompt->setPrice($_POST['price']);
+            $prompt->setDetails($_POST['details']);
+            $prompt->setUserId($_SESSION['user_id']);
+            $prompt->save();
+        }
+        catch(Throwable $e){
+            $error = $e->getMessage();
+        }
+        
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,14 +74,58 @@
         <?php endif; ?>
     <h1>Homepage</h1>
 
+    <!-- Toont formulier om prompt toe te voegen -->
+    <?php if($_SESSION['loggedin'] === true): ?>
+        <form action="" method="post" enctype="multipart/form-data">
+            <?php if( isset($error) ):?>
+                <div class="form__error">
+                    <p>
+                        <?php echo $error;?>
+                    </p>
+                </div>
+            <?php endif; ?>
+            <div class="form__field">
+                <label for="prompt">Prompt</label>
+                <input type="text" name="prompt">
+            </div>
+            <div class="form__field">
+                <label for="image">Upload image</label>
+                <input type="file" name="image">
+            </div>
+            <div class="form__field">
+                <label for="price">Select price</label>
+                <select name="price">
+                    <option value="free">Free</option>
+                    <option value="1 credit">1 credit</option>
+                    <option value="2 credits">2 credits</option>
+                </select>
+            </div>
+            <div class="form__field">
+                <label for="details">Type and details of model</label>
+                <input type="text" name="details">
+            </div>
+            <div class="form__field">
+                <input type="submit" value="Add" class="btn btn--primary">  
+            </div>
+        </form>
+    <?php else :
+        echo "<p>Login to add a prompt</p>";
+    endif; ?>
+    
+
+    <!-- Toont prompts -->
     <div class="prompts">
         <?php foreach($prompts as $prompt): ?>
             <div class="prompt">
                 <?php if($_SESSION['loggedin'] !== true): ?>
-                    <p><strong><?php echo htmlspecialchars($prompt['firstname']) . " " . htmlspecialchars($prompt['lastname']);?></strong> <?php echo htmlspecialchars(substr($prompt['prompt'], 0, 20)) . '...';?></p>
+                    <p><strong><?php echo htmlspecialchars($prompt['firstname']) . " " . htmlspecialchars($prompt['lastname']);?></strong> <?php echo "prompt: " . htmlspecialchars(substr($prompt['prompt'], 0, 20)) . '...';?></p>
                     <a href="login.php">login to see full prompt</a>
                 <?php else: ?>
-                    <p><strong><?php echo htmlspecialchars($prompt['firstname']) . " " . htmlspecialchars($prompt['lastname']);;?></strong> <?php echo htmlspecialchars($prompt['prompt']);?></p>
+                    <strong><?php echo htmlspecialchars($prompt['firstname']) . " " . htmlspecialchars($prompt['lastname']);?></strong>
+                    <p><?php echo "prompt: " . htmlspecialchars($prompt['prompt']);?></p>
+                    <img src="uploads/<?php echo htmlspecialchars($prompt['image']);?>" alt="prompt image">
+                    <p><?php echo "price: " . htmlspecialchars($prompt['price']);?></p>
+                    <p><?php echo "details: " . htmlspecialchars($prompt['details']);?></p>
                 <?php endif; ?>
             </div>
         <?php endforeach; ?>
--- src/PromptPlaza/Framework/Prompt.php
+++ src/PromptPlaza/Framework/Prompt.php
@@ -4,9 +4,79 @@ namespace PromptPlaza\Framework;
 
 class Prompt
 {
-    public int $id;
-    public string $prompt;
-    public int $user_id;
+    private $id;
+    private $prompt;
+    private $user_id;
+    private $image;
+    private $price;
+    private $details;
+
+    public function getPrompt()
+    {
+        return $this->prompt;
+    }
+
+    public function setPrompt($prompt)
+    {
+        if (empty($prompt)) {
+            throw new \Exception("Prompt cannot be empty.");
+        } else {
+            $this->prompt = $prompt;
+            return $this;
+        }
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image)
+    {
+        if (empty($image)) {
+            throw new \Exception("Image cannot be empty.");
+        } else {
+            $this->image = $image;
+            return $this;
+        }
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        if (empty($price)) {
+            throw new \Exception("Price cannot be empty.");
+        } else {
+            $this->price = $price;
+            return $this;
+        }
+    }
+
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    public function setDetails($details)
+    {
+        if (empty($details)) {
+            throw new \Exception("Details cannot be empty.");
+        } else {
+            $this->details = $details;
+            return $this;
+        }
+    }
+
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+        return $this;
+    }
+
 
     //haalt alle prompts op en geeft ze terug
     public static function getAll($offset = 0){
@@ -29,5 +99,16 @@ class Prompt
         $statement = $conn->prepare("SELECT COUNT(*) FROM prompts");
         $statement->execute();
         return $statement->fetchColumn();
+    }
+
+    public function save(){
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("INSERT INTO prompts (prompt, user_id, image, price, details) VALUES (:prompt, :user_id, :image, :price, :details)");
+        $statement->bindValue(":prompt", $this->prompt);
+        $statement->bindValue(":user_id", $this->user_id);
+        $statement->bindValue(":image", $this->image);
+        $statement->bindValue(":price", $this->price);
+        $statement->bindValue(":details", $this->details);
+        return $statement->execute();
     }
 }
