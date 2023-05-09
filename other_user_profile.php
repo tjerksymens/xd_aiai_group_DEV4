bb33259db63bbc42d5dcb35d64d8efaef72437d3
wouter856 <wouterwaumans@live.be>
2023-05-09 13:11:23 +0200

    feat: users can view other users prompts and own prompts true the profile page

--- other_user_profile.php
+++ other_user_profile.php
@@ -15,6 +15,12 @@ $cloudinary = new Cloudinary(
     ]
 );
 
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+$offset = ($page - 1) * 10;
+$prompts = \PromptPlaza\Framework\Prompt::getAll($offset);
+$totalPrompts = \PromptPlaza\Framework\Prompt::countAll();
+$totalPages = ceil($totalPrompts / 10);
+
 //ga naar home als er geen profilename is meegegeven
 if (!isset($_GET['username'])) {
     header('location: index.php');
@@ -22,9 +28,9 @@ if (!isset($_GET['username'])) {
 
 $user = \PromptPlaza\Framework\User::getByUsername($_GET['username']);
 //ga naar eigen profiel als je op je eigen naam klikt
-if($_GET['username'] == $user['username']){
+/*if($_GET['username'] == $user['username']){
     header('location: profile.php');
-}
+}*/
 $profile_picture = $user['image'];
 
 ?>
@@ -59,6 +65,82 @@ $profile_picture = $user['image'];
             </p>
         </div>
     <?php endif; ?>
+
+    <h2>Prompts by this user</h2>
+
+    <!-- Toont zoeken op details -->
+    <form action="" method="get">
+        <label for="details">Browse user prompts by details</label>
+        <input type="text" name="details">
+        <input type="submit" value="Browse" class="btn btn--primary">
+    </form>
+
+    <!-- Toont prompts -->
+    <div class="prompts">
+    <?php foreach ($prompts as $prompt) : ?>
+            <?php if ($prompt['username'] == $user['username']) : ?>
+                <div class="prompt">
+                    <strong><a href="other_user_profile.php?username=<?php echo htmlspecialchars($prompt['username']); ?>"><?php echo htmlspecialchars($prompt['username']); ?></a></strong>
+                    <p><?php echo "prompt: " . htmlspecialchars($prompt['prompt']); ?></p>
+                    <img src="<?php echo $cloudinary->image($prompt['image'])->resize(Resize::fill(100, 150))->toUrl(); ?>" alt="prompt image">
+                    <p><?php echo "price: " . htmlspecialchars($prompt['price']); ?></p>
+                    <p><?php echo "details: " . htmlspecialchars($prompt['details']); ?></p>
+
+                    <!-- Toont likes-->
+                    <div>
+                        <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="like">Like</a>
+                        <span class='likes' id="likes<?php echo htmlspecialchars($prompt['id']) ?>"><?php echo $prompts = \PromptPlaza\Framework\Prompt::getLikes($prompt['id']); ?></span>
+                        <span class="status"></span>
+                        people like this
+                    </div>
+
+                    <!-- Toont add to favourite -->
+                    <div>
+                        <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="favourite_add" id="favourite_add<?php echo htmlspecialchars($prompt['id']) ?>">Add to favourites</a>
+                    </div>
+
+
+                    <!-- Toont comments -->
+                    <div class="post_comments">
+                        <div class="post_comments_form">
+                            <input type="text" placeholder="Place here your comment" id="comment<?php echo htmlspecialchars($prompt['id']) ?>">
+                            <a href="#" class="btn_comments" data-id="<?php echo htmlspecialchars($prompt['id']) ?>">Add comment</a>
+                        </div>
+
+                        <ul class="post_comments_list<?php echo htmlspecialchars($prompt['id']) ?>">
+                            <?php $allComments = \PromptPlaza\Framework\Comment::getComments($prompt['id']);
+                            foreach ($allComments as $c) : ?>
+                                <li>
+                                    <strong><a href="other_user_profile.php?username=<?php echo htmlspecialchars($c['username']); ?>"><?php echo htmlspecialchars($c['username']); ?></a></strong>
+                                    <?php echo $c['text']; ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
+
+    <!-- Teller om van pagina te veranderen voor volgende prompts te zien -->
+    <div class="pagination">
+        <?php if ($page > 1) : ?>
+            <a href="?page=<?php echo $page - 1; ?>">Previous</a>
+        <?php endif; ?>
+
+        <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+            <?php if ($i === $page) : ?>
+                <span><?php echo $i; ?></span>
+            <?php else : ?>
+                <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+            <?php endif; ?>
+        <?php endfor; ?>
+
+        <?php if ($page < $totalPages) : ?>
+            <a href="?page=<?php echo $page + 1; ?>">Next</a>
+        <?php endif; ?>
+    </div>
 </body>
 
 </html>
--- profile.php
+++ profile.php
@@ -15,6 +15,12 @@ $cloudinary = new Cloudinary(
     ]
 );
 
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+$offset = ($page - 1) * 10;
+$prompts = \PromptPlaza\Framework\Prompt::getAll($offset);
+$totalPrompts = \PromptPlaza\Framework\Prompt::countAll();
+$totalPages = ceil($totalPrompts / 10);
+
 $user_id = $_SESSION['user_id'];
 $user = \PromptPlaza\Framework\User::getById($user_id);
 $profile_picture = $user['image'];
@@ -106,6 +112,82 @@ if (!empty($_POST)) {
     <form action="" method="post">
         <button type="submit" name="delete_account">Delete Account</button>
     </form>
+
+    <h2>Your prompts</h2>
+
+    <!-- Toont zoeken op details -->
+    <form action="" method="get">
+        <label for="details">Browse your prompts by details</label>
+        <input type="text" name="details">
+        <input type="submit" value="Browse" class="btn btn--primary">
+    </form>
+
+    <!-- Toont prompts -->
+    <div class="prompts">
+    <?php foreach ($prompts as $prompt) : ?>
+            <?php if ($prompt['username'] == $user['username']) : ?>
+                <div class="prompt">
+                    <strong><a href="other_user_profile.php?username=<?php echo htmlspecialchars($prompt['username']); ?>"><?php echo htmlspecialchars($prompt['username']); ?></a></strong>
+                    <p><?php echo "prompt: " . htmlspecialchars($prompt['prompt']); ?></p>
+                    <img src="<?php echo $cloudinary->image($prompt['image'])->resize(Resize::fill(100, 150))->toUrl(); ?>" alt="prompt image">
+                    <p><?php echo "price: " . htmlspecialchars($prompt['price']); ?></p>
+                    <p><?php echo "details: " . htmlspecialchars($prompt['details']); ?></p>
+
+                    <!-- Toont likes-->
+                    <div>
+                        <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="like">Like</a>
+                        <span class='likes' id="likes<?php echo htmlspecialchars($prompt['id']) ?>"><?php echo $prompts = \PromptPlaza\Framework\Prompt::getLikes($prompt['id']); ?></span>
+                        <span class="status"></span>
+                        people like this
+                    </div>
+
+                    <!-- Toont add to favourite -->
+                    <div>
+                        <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="favourite_add" id="favourite_add<?php echo htmlspecialchars($prompt['id']) ?>">Add to favourites</a>
+                    </div>
+
+
+                    <!-- Toont comments -->
+                    <div class="post_comments">
+                        <div class="post_comments_form">
+                            <input type="text" placeholder="Place here your comment" id="comment<?php echo htmlspecialchars($prompt['id']) ?>">
+                            <a href="#" class="btn_comments" data-id="<?php echo htmlspecialchars($prompt['id']) ?>">Add comment</a>
+                        </div>
+
+                        <ul class="post_comments_list<?php echo htmlspecialchars($prompt['id']) ?>">
+                            <?php $allComments = \PromptPlaza\Framework\Comment::getComments($prompt['id']);
+                            foreach ($allComments as $c) : ?>
+                                <li>
+                                    <strong><a href="other_user_profile.php?username=<?php echo htmlspecialchars($c['username']); ?>"><?php echo htmlspecialchars($c['username']); ?></a></strong>
+                                    <?php echo $c['text']; ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
+
+    <!-- Teller om van pagina te veranderen voor volgende prompts te zien -->
+    <div class="pagination">
+        <?php if ($page > 1) : ?>
+            <a href="?page=<?php echo $page - 1; ?>">Previous</a>
+        <?php endif; ?>
+
+        <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+            <?php if ($i === $page) : ?>
+                <span><?php echo $i; ?></span>
+            <?php else : ?>
+                <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+            <?php endif; ?>
+        <?php endfor; ?>
+
+        <?php if ($page < $totalPages) : ?>
+            <a href="?page=<?php echo $page + 1; ?>">Next</a>
+        <?php endif; ?>
+    </div>
 </body>
 
 </html>
