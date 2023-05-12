84faf3b0132080b79b212ef84d39c1d8a33ace0b
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-05-12 22:59:14 +0200

    update: root colors and font + @media

--- nav.inc.php
+++ nav.inc.php
@@ -1,8 +1,10 @@
 <div id="nav">
-    <a href="index.php">
-        <h1 class="logo">Promptplaza</h1>
-    </a>
-    <a href="profile.php">Profile</a>
-    <a href="favourites.php">Favourites</a>
-    <a href="logout.php" id="logout_button">Logout</a>
+    <div class="container">
+        <a href="index.php">
+            <h1 class="logo">Promptplaza</h1>
+        </a>
+        <a href="profile.php">Profile</a>
+        <a href="favourites.php">Favourites</a>
+        <a href="logout.php" id="logout_button">Logout</a>
+    </div>
 </div>
