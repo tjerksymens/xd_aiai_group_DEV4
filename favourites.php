b04d21147fbc4bcb311377ae0d054759eb4f8db7
wouter856 <wouterwaumans@live.be>
2023-05-13 16:19:00 +0200

    fix: loads heads up text when you don't have any favourites yet on the favourites page

--- favourites.php
+++ favourites.php
@@ -39,58 +39,62 @@ $prompts = \PromptPlaza\Framework\Prompt::getAllFavourites($_SESSION['user_id'])
     <h1>These are your favourites!</h1>
     <!-- Toont prompts -->
     <div class="prompts">
-        <?php foreach ($prompts as $prompt) : ?>
-            <div class="prompt">
-                <strong id="Prompt__Creator__Head">Made by: <a href="other_user_profile.php?username=<?php echo htmlspecialchars($prompt['username']); ?>"><?php echo htmlspecialchars($prompt['username']); ?></a></strong>
-                <h2><?php echo "prompt: " . htmlspecialchars($prompt['prompt']); ?></h2>
-                <img src="<?php echo $cloudinary->image($prompt['image'])->resize(Resize::fill(100, 150))->toUrl(); ?>" alt="prompt image">
-                <div id="Prompt__Details">
-                    <?php if ($prompt['price'] == 1) : ?>
-                        <p><?php echo "price: " . htmlspecialchars($prompt['price'])  . " credit"; ?></p>
-                    <?php else : ?>
-                        <p><?php echo "price: " . htmlspecialchars($prompt['price'])  . " credits"; ?></p>
-                    <?php endif; ?>
-                    <p><?php echo "details: " . htmlspecialchars($prompt['details']); ?></p>
-                </div>
-
-                <div id="Prompt__LikeFavourite">
-                    <!-- Toont likes-->
-                    <div>
-                        <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="like" id="like<?php echo htmlspecialchars($prompt['id']) ?>">Like</a>
-                        <span class='likes' id="likes<?php echo htmlspecialchars($prompt['id']) ?>"><?php echo $prompts = \PromptPlaza\Framework\Prompt::getLikes($prompt['id']); ?></span>
-                        <?php if ($prompts !== 1) : ?>
-                            <span class="status">people like this</span>
+        <?php if (empty($prompts)) : ?>
+            <p>You have no favourites yet. You can add some true any prompt screen.</p>
+        <?php else : ?>
+            <?php foreach ($prompts as $prompt) : ?>
+                <div class="prompt">
+                    <strong id="Prompt__Creator__Head">Made by: <a href="other_user_profile.php?username=<?php echo htmlspecialchars($prompt['username']); ?>"><?php echo htmlspecialchars($prompt['username']); ?></a></strong>
+                    <h2><?php echo "prompt: " . htmlspecialchars($prompt['prompt']); ?></h2>
+                    <img src="<?php echo $cloudinary->image($prompt['image'])->resize(Resize::fill(100, 150))->toUrl(); ?>" alt="prompt image">
+                    <div id="Prompt__Details">
+                        <?php if ($prompt['price'] == 1) : ?>
+                            <p><?php echo "price: " . htmlspecialchars($prompt['price'])  . " credit"; ?></p>
                         <?php else : ?>
-                            <span class="status">person likes this</span>
+                            <p><?php echo "price: " . htmlspecialchars($prompt['price'])  . " credits"; ?></p>
                         <?php endif; ?>
+                        <p><?php echo "details: " . htmlspecialchars($prompt['details']); ?></p>
                     </div>
 
-                    <!-- Toont add to favourite -->
-                    <div>
-                        <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="favourite" id="favourite<?php echo htmlspecialchars($prompt['id']) ?>">Add to favourites</a>
+                    <div id="Prompt__LikeFavourite">
+                        <!-- Toont likes-->
+                        <div>
+                            <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="like" id="like<?php echo htmlspecialchars($prompt['id']) ?>">Like</a>
+                            <span class='likes' id="likes<?php echo htmlspecialchars($prompt['id']) ?>"><?php echo $prompts = \PromptPlaza\Framework\Prompt::getLikes($prompt['id']); ?></span>
+                            <?php if ($prompts !== 1) : ?>
+                                <span class="status">people like this</span>
+                            <?php else : ?>
+                                <span class="status">person likes this</span>
+                            <?php endif; ?>
+                        </div>
+
+                        <!-- Toont add to favourite -->
+                        <div>
+                            <a href="#" data-id="<?php echo htmlspecialchars($prompt['id']) ?>" class="favourite" id="favourite<?php echo htmlspecialchars($prompt['id']) ?>">Add to favourites</a>
+                        </div>
                     </div>
+
+                    <!-- Toont comments -->
+                    <div class="post_comments">
+                        <div class="post_comments_form">
+                            <input type="text" placeholder="Place your comment here" id="comment<?php echo htmlspecialchars($prompt['id']) ?>">
+                            <a href="#" class="btn_comments" data-id="<?php echo htmlspecialchars($prompt['id']) ?>">Add comment</a>
+                        </div>
+
+                        <ul class="post_comments_list<?php echo htmlspecialchars($prompt['id']) ?>">
+                            <?php $allComments = \PromptPlaza\Framework\Comment::getComments($prompt['id']);
+                            foreach ($allComments as $c) : ?>
+                                <li>
+                                    <strong><a href="other_user_profile.php?username=<?php echo htmlspecialchars($c['username']); ?>"><?php echo htmlspecialchars($c['username']); ?></a></strong>
+                                    <?php echo $c['text']; ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
                 </div>
-
-                <!-- Toont comments -->
-                <div class="post_comments">
-                    <div class="post_comments_form">
-                        <input type="text" placeholder="Place your comment here" id="comment<?php echo htmlspecialchars($prompt['id']) ?>">
-                        <a href="#" class="btn_comments" data-id="<?php echo htmlspecialchars($prompt['id']) ?>">Add comment</a>
-                    </div>
-
-                    <ul class="post_comments_list<?php echo htmlspecialchars($prompt['id']) ?>">
-                        <?php $allComments = \PromptPlaza\Framework\Comment::getComments($prompt['id']);
-                        foreach ($allComments as $c) : ?>
-                            <li>
-                                <strong><a href="other_user_profile.php?username=<?php echo htmlspecialchars($c['username']); ?>"><?php echo htmlspecialchars($c['username']); ?></a></strong>
-                                <?php echo $c['text']; ?>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 
     <script src="js/script.js"></script>
