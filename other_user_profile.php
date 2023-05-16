fe3eb7d9a4d4ee25c182f44bd145a58354a24fc8
wouter856 <wouterwaumans@live.be>
2023-05-16 13:47:18 +0200

    feat: buy button added to other users and to favourites

--- other_user_profile.php
+++ other_user_profile.php
@@ -1,6 +1,7 @@
 <?php
 include_once(__DIR__ . "/bootstrap.php");
 $config = parse_ini_file("config/config.ini");
+$apiKey = $config['SENDGRID_API_KEY'];
 
 use Cloudinary\Cloudinary;
 use Cloudinary\Transformation\Resize;
@@ -48,6 +49,55 @@ if ($follow->checkIfFollowing($_SESSION['user_id'], $followedId['id']) == true) 
     $followed = true;
 } else {
     $followed = false;
+}
+
+if (isset($_POST['buy_prompt'])) {
+    $price = \PromptPlaza\Framework\Prompt::getPriceById($_POST['buy_prompt']);
+    $credits = \PromptPlaza\Framework\User::getCredits($_SESSION['user_id']);
+    $prompt = \PromptPlaza\Framework\Prompt::getById($_POST['buy_prompt']);
+    if ($prompt['user_id'] == $_SESSION['user_id']) {
+        $error = "You own this prompt.";
+    } else {
+        if ($credits < $price) {
+            $error = "You don't have enough credits to buy this prompt.";
+        } else {
+            //credits updaten
+            $user = \PromptPlaza\Framework\User::updateCreditsById($_SESSION['user_id'], $price['price']);
+            
+            //prompt kopen
+            $promptId = $_POST['buy_prompt'];
+            $buy = new \PromptPlaza\Framework\Bought();
+            $buy->setPromptId($promptId);
+            $buy->setUserId($_SESSION['user_id']);
+            $buy->save(); 
+
+            $user = \PromptPlaza\Framework\User::getById($_SESSION['user_id']);
+            $fullname = $user['firstname'] . " " . $user['lastname'];
+            $firstname = $user['firstname'];
+            $title = $prompt['prompt'];
+            //zend mail met de prompt
+            $email = new \SendGrid\Mail\Mail(); // create new email
+			$email->setFrom("promptplaza@hotmail.com", "Wouter From Promptplaza"); // set sender
+			$email->setSubject("Here is your prompt"); // set subject
+			$email->addTo($user['email'], $fullname); // set recipient
+			$email->addContent("text/plain", "Hey $firstname! Thank you for your purchase! <br> 
+                Here is your new prompt: <strong>$title</strong> <br> <br> We hope you will have fun with it and come back for more prompts at Promtplaza."); 
+			$email->addContent(
+				"text/html",
+				"Hey $firstname! Thank you for your purchase! <br> 
+                Here is your new prompt: <strong>$title</strong> <br> <br> We hope you will have fun with it and come back for more prompts at Promtplaza."
+			); //set text
+			$sendgrid = new \SendGrid($apiKey);
+			try { // try to send email
+				$response = $sendgrid->send($email);
+				print $response->statusCode() . "\n";
+				print_r($response->headers());
+				print $response->body() . "\n";
+			} catch (Exception $e) { // if email could not be sent, print error
+				echo 'Caught exception: ' . $e->getMessage() . "\n";
+			}
+        }
+    }
 }
 
 $user = \PromptPlaza\Framework\User::getByUsername($_GET['username']);
@@ -114,7 +164,18 @@ $profile_picture = $user['image'];
                     <strong><a href="other_user_profile.php?username=<?php echo htmlspecialchars($prompt['username']); ?>"><?php echo htmlspecialchars($prompt['username']); ?></a></strong>
                     <p><?php echo "prompt: " . htmlspecialchars($prompt['prompt']); ?></p>
                     <img src="<?php echo $cloudinary->image($prompt['image'])->resize(Resize::fill(100, 150))->toUrl(); ?>" alt="prompt image">
-                    <p><?php echo "price: " . htmlspecialchars($prompt['price']); ?></p>
+                    <?php if ($prompt['price'] == 1) : ?>
+                        <p><?php echo "price: " . htmlspecialchars($prompt['price'])  . " credit"; ?></p>
+                    <?php else : ?>
+                        <p><?php echo "price: " . htmlspecialchars($prompt['price'])  . " credits"; ?></p>
+                    <?php endif; ?>
+                    <?php if (!$showBuyButton = \PromptPlaza\Framework\Bought::checkIfBought($_SESSION['user_id'], $prompt['id'])) : ?>
+                        <form action="" method="post">
+                            <button type="submit" name="buy_prompt" value="<?php echo htmlspecialchars($prompt['id']) ?>">Buy</button>
+                        </form>
+                    <?php else : ?>
+                        <p>Owned</p>
+                    <?php endif; ?>
                     <p><?php echo "details: " . htmlspecialchars($prompt['details']); ?></p>
 
                     <!-- Toont likes-->
