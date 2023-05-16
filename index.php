5f40691de0a2b84e694b1b532a300f85dafc425e
wouter856 <wouterwaumans@live.be>
2023-05-16 16:27:55 +0200

    fix: can now only buy prompts with sufficient credits or free prompts

--- index.php
+++ index.php
@@ -53,12 +53,7 @@ if (isset($_POST['buy_prompt'])) {
     if ($prompt['user_id'] == $_SESSION['user_id']) {
         $error = "You own this prompt.";
     } else {
-        if ($credits < $price) {
-            $error = "You don't have enough credits to buy this prompt.";
-        } else {
-            //credits updaten
-            $user = \PromptPlaza\Framework\User::updateCreditsById($_SESSION['user_id'], $price['price']);
-
+        if ($price['price'] === 0) {
             //prompt kopen
             $promptId = $_POST['buy_prompt'];
             $buy = new \PromptPlaza\Framework\Bought();
@@ -88,6 +83,45 @@ if (isset($_POST['buy_prompt'])) {
                 $responseData = $response;
             } catch (Exception $e) { // if email could not be sent, print error
                 echo 'Caught exception: ' . $e->getMessage() . "\n";
+            }
+        }
+        else {
+            if ($credits['credits'] < $price['price']) {
+                $error = "You don't have enough credits to buy this prompt.";
+            } else {
+                //credits updaten
+                $user = \PromptPlaza\Framework\User::updateCreditsById($_SESSION['user_id'], $price['price']);
+    
+                //prompt kopen
+                $promptId = $_POST['buy_prompt'];
+                $buy = new \PromptPlaza\Framework\Bought();
+                $buy->setPromptId($promptId);
+                $buy->setUserId($_SESSION['user_id']);
+                $buy->save();
+    
+                $user = \PromptPlaza\Framework\User::getById($_SESSION['user_id']);
+                $fullname = $user['firstname'] . " " . $user['lastname'];
+                $firstname = $user['firstname'];
+                $title = $prompt['prompt'];
+                //zend mail met de prompt
+                $email = new \SendGrid\Mail\Mail(); // create new email
+                $email->setFrom("promptplaza@hotmail.com", "Wouter From Promptplaza"); // set sender
+                $email->setSubject("Here is your prompt"); // set subject
+                $email->addTo($user['email'], $fullname); // set recipient
+                $email->addContent("text/plain", "Hey $firstname! Thank you for your purchase! <br> 
+                    Here is your new prompt: <strong>$title</strong> <br> <br> We hope you will have fun with it and come back for more prompts at Promtplaza.");
+                $email->addContent(
+                    "text/html",
+                    "Hey $firstname! Thank you for your purchase! <br> 
+                    Here is your new prompt: <strong>$title</strong> <br> <br> We hope you will have fun with it and come back for more prompts at Promtplaza."
+                ); //set text
+                $sendgrid = new \SendGrid($apiKey);
+                try { // try to send email
+                    $response = $sendgrid->send($email);
+                    $responseData = $response;
+                } catch (Exception $e) { // if email could not be sent, print error
+                    echo 'Caught exception: ' . $e->getMessage() . "\n";
+                }
             }
         }
     }
